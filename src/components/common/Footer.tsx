e7bd7d6a6477903d09c96c0d5ec6c95312770601
sheghurahmathullah <sheghu379@gmail.com>
2025-11-15 12:57:14 +0530

    footer link update

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -71,6 +71,25 @@ const socialIcons = {
       <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
     </svg>
   ),
+  linkedin: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="24px"
+      height="24px"
+      viewBox="0 0 48 48"
+    >
+      <path
+        fill="#0288D1"
+        d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
+      ></path>
+      <path
+        fill="#FFF"
+        d="M12,19h5v17h-5V19z M16.485,17h-0.028C12.965,17,12,15.888,12,14.499C12,13.08,12.995,12,14.514,12 c1.521,0,2.458,1.08,2.486,2.499C17,15.887,16.035,17,16.485,17z M39,36h-5v-9.099c0-2.198-0.814-3.699-2.763-3.699 C30.009,23.201,29,24.508,29,26.601V36h-5V19h5v2.616C29.721,20.5,30.85,19,33.738,19C37.229,19,39,21.477,39,26.9V36z"
+      ></path>
+    </svg>
+  ),
 };
 
 const Footer = () => {
@@ -110,14 +129,14 @@ const Footer = () => {
 
             {/* Social Icons */}
             <div className="flex items-center gap-3 mt-6">
-              <a
-                href="https://www.facebook.com/gokite"
+              {/* <a
+                href="https://www.facebook.com/gokitetravelandtours/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-white/50 flex items-center justify-center hover:bg-white/70 transition-colors"
               >
                 {socialIcons.facebook}
-              </a>
+              </a> */}
               <a
                 href="https://www.instagram.com/gokite"
                 target="_blank"
@@ -127,7 +146,7 @@ const Footer = () => {
                 {socialIcons.instagram}
               </a>
               <a
-                href="https://twitter.com/gokite"
+                href="https://x.com/Gokitetravel"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-white/50 flex items-center justify-center hover:bg-white/70 transition-colors"
@@ -135,13 +154,21 @@ const Footer = () => {
                 {socialIcons.twitter}
               </a>
               <a
+                href="https://www.linkedin.com/company/gokitetravelandtours/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 rounded-full bg-white/50 flex items-center justify-center hover:bg-white/70 transition-colors"
+              >
+                {socialIcons.linkedin}
+              </a>
+              {/* <a
                 href="https://wa.me/917620370639"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-white/50 flex items-center justify-center hover:bg-white/70 transition-colors"
               >
                 {socialIcons.whatsapp}
-              </a>
+              </a> */}
             </div>
           </div>
 
